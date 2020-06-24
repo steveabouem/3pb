422b79a265be62d8f8e8ba9fddb53a6d3d0f87b1
Dylan marcel <marcel.w.dylan@gmail.com>
2020-06-24 23:37:19 +0200

    resolution function

--- directions/functions/index.js
+++ directions/functions/index.js
@@ -54,6 +54,58 @@ exports.getUser = functions.https.onRequest((req, res) => {
 
 });
 
+exports.deleteUser = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        const {email} = req.body;
+        users.doc(email).get().then(function(doc) {
+            if (doc.exists) {
+               // 
+            } 
+        }).catch(function(error) {
+            res.send({code: 500, data: error});
+        });
+    });
+
+    });
+});
+// TODO: handles password and sessions with custom tokens: https://firebase.google.com/docs/auth/admin/verify-id-tokens
+exports.createUser = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        const {email, password} = req.body;
+        const id = uuidv4();
+
+        users.doc(id).set({email,  created: moment(), maps: null, imageUrl: null, id})
+        .then(() => {
+            users.doc(id).get().then(function(doc) {
+                if (doc.exists) {
+                    res.send(doc.data());
+                } else {
+                    res.send('None found');
+                }
+            }).catch(function(error) {
+                res.send('Error', error);
+            });
+        })
+    });
+
+});
+
+exports.getUser = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        const {email} = req.body;
+        users.doc(email).get().then(function(doc) {
+            if (doc.exists) {
+                res.send({code: 200, data: doc.data()});
+            } else {
+                res.send({code: 200, data: null});
+            }
+        }).catch(function(error) {
+            res.send({code: 500, data: error});
+        });
+    });
+
+});
+
 // cette fonction attend les arguments suivants: 
 // {
 //     mapName: string, obligatoire
@@ -116,4 +168,61 @@ exports.getMaps = functions.https.onRequest((req, res) => {
                 
             });
         });
+});
+
+// Methode permetttant authentification (temporaire)
+//  
+//  require users(email, password);
+//  check dans la databese les informations
+//  
+exports.authLogin = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        users.get(email,password)
+        .then(doc =>{
+            if(doc.exists){
+                if(users.doc(email) == loginEmail && users.doc(password) == loginPassword){
+                    console.log("valide");
+                }else{
+                    console.log("le mots de passe ou l'email");
+                }
+                console.log("users existe pas");
+            }
+        })
+
+    });
+});
+// Methode permetttant authentification (temporaire)
+//  
+//  require users(email, password);
+//  check dans la databese les informations
+//  
+exports.shareImage = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        //const signIn = firebase.auth().signInWithEmailAndPassword(email,password);
+
+        maps.get();
+        users.get(email,password)
+            .then(doc =>{
+                if(doc.exists){
+                    if(users.doc(email) != signEmail && users.doc(password) != signPassword){  // changer quand le systeme de session mis en place
+                        console.log("refuser le partage image")
+                    }else{
+                        console.log("accepté le partage image")
+                    }
+                    console.log("users existe pas")
+                }
+            })
+    });
+});
+
+exports.checkSignIn = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function(result) {
+        // result.user.tenantId should be ‘TENANT_PROJECT_ID’.
+    }).catch(function(error) {
+        // Handle error.
+    });
+       
+    });
 });
